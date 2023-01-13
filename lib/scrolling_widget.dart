b63f32ce5121ffb292e618c9303aeaa6c8664e2a
Dolph Hincapie <dbhincap@bancolombia.com.co>
2023-01-13 15:04:18 -0500

    Documentos principal

---
+++ lib/scrolling_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp(
+    items: List<String>.generate(10000, (i) => 'Item $i'),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  final List<String> items;
+
+  const MyApp({super.key, required this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Long List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: ListView.builder(
+          key: const Key('long_list'),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(
+                items[index],
+                key: Key('item_${index}_text'),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
